b418fad48de71e1642640fa38b48aee70d153079
Brian Machado <bmachado@live.ca>
2024-08-13 20:20:58 -0700

    control fluid velocity

--- syringepump/teensycontrol.ino
+++ syringepump/teensycontrol.ino
@@ -1,34 +1,35 @@
 #define PULSE_PIN 3
 #define DIR_PIN 2
 
-bool FORWARD;
+const int STEPS_PER_REVOLUTION = 6400;
+const float LEAD_SCREW_PITCH_MM = 1.0;
+
+const float SYRINGE_DIAMETER_MM = 9.0;
+const float TUBE_DIAMETER_MM = 1.0; // Change this to match your tube
+
+float FLUID_VELOCITY_MM_PER_SEC = 1; // Desired fluid velocity in mm/s
+
+float steps_per_second;
+float plunger_velocity_mm_per_sec;
 
 void setup() {
   pinMode(PULSE_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
 
-  FORWARD=true;
+  float area_ratio = pow(SYRINGE_DIAMETER_MM / TUBE_DIAMETER_MM, 2);
+  
+  plunger_velocity_mm_per_sec = FLUID_VELOCITY_MM_PER_SEC / area_ratio;
+  
+  steps_per_second = plunger_velocity_mm_per_sec * (STEPS_PER_REVOLUTION / LEAD_SCREW_PITCH_MM);
+  
+  digitalWrite(DIR_PIN, LOW); // Set direction (adjust as needed)
 }
 
 void loop() {
-  if(FORWARD){
-    digitalWrite(DIR_PIN, HIGH);
-    FORWARD=false;
-  }
-  else{
-    digitalWrite(DIR_PIN, LOW);
-    FORWARD=true;
-  }
-
-  delay(1000);
-
-  int steps = 10000;
-  for(int i=0; i<steps; i++){
-    digitalWrite(PULSE_PIN, HIGH);
-    delayMicroseconds(2000);
-    digitalWrite(PULSE_PIN, LOW);
-    delayMicroseconds(2000);
-  }
+  long step_delay_micros = (1000000 / steps_per_second) / 2; // Divide by 2 for high and low pulse
+  
+  digitalWrite(PULSE_PIN, HIGH);
+  delayMicroseconds(step_delay_micros);
+  digitalWrite(PULSE_PIN, LOW);
+  delayMicroseconds(step_delay_micros);
 }
-
-
